da9a216141e00fc03c1c2c45daaf1cd7f61ae816
David Lighty <david.lighty@gmail.com>
2018-11-24 17:41:51 -0800

    theory

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,23 @@
+import theoretically from 'jest-theories'
 import { fun } from '.'
 
 describe('module tests', () => {
   describe('fun tests', () => {
     it('will return a fun string', () => {
       const expected = 'fun is fun.'
-      const test = fun('fun')
-      expect(test).toBe(expected)
+      const output = fun('fun')
+      expect(output).toBe(expected)
+    })
+
+    const theories = [
+      { input: null, expected: 'null is fun.' },
+      { input: ' ', expected: '  is fun.' },
+      { input: 'Theories', expected: 'Theories is fun.' }
+    ]
+
+    theoretically('the {input} beomes a fun string', theories, theory => {
+      const output = fun(theory.input)
+      expect(output).toBe(theory.expected)
     })
   })
 })
